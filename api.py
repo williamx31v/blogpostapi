cd3a77c00bd561473eb2b6c57bdbbf3cd61c949f
williamx31v <williamx@easystreet.net>
2016-12-01 18:53:27 -0800

    Add files via upload

--- api.py
+++ api.py
@@ -3,9 +3,8 @@
 #define modules for import
 import sqlite3
 import json
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, request, jsonify
+#from flask import request
 
 #define app
 app = Flask(__name__)
@@ -13,28 +12,30 @@ app = Flask(__name__)
 #set the database path
 DATABASE='/home/william/blogpostapi/blog.db'
 
+#define the api post endpoint
+@app.route('/blogposts/api/v1.0/post', methods=['POST'])
+def post():
+	if not request.json:
+		abort(400)
+	new_post = request.json
+	columns = ', '.join(new_post.keys())
+	vals = '"'+'", "'.join(new_post.values())
+	query = 'INSERT INTO posts (%s) VALUES (%s")' % (columns, vals)
+	con=sqlite3.connect(DATABASE)
+	c=con.cursor()
+	c.execute(query)
+	con.commit()
+	con.close
+	return "OK"
 
-@app.route('/blogposts/api/v1.0/posts', methods=['GET','POST'])
-def result():
-	if request.method == 'POST':
-		new_post = request.json
-		columns = ', '.join(new_post.keys())
-		vals = '"'+'", "'.join(new_post.values())
-		query = 'INSERT INTO posts (%s) VALUES (%s")' % (columns, vals)
-		con=sqlite3.connect(DATABASE)
-		c=con.cursor()
-		c.execute(query)
-		con.commit()
-		con.close
-		return "OK"
-
-	else:
-		con=sqlite3.connect(DATABASE)
-		c=con.cursor()
-		c.execute("SELECT * from posts")
-		json_get=json.dumps(c.fetchall())
-		con.close
-		return json_get
+@app.route('/blogposts/api/v1.0/posts', methods=['GET'])
+def get():
+	con=sqlite3.connect(DATABASE)
+	c=con.cursor()
+	c.execute('SELECT * from posts')
+	json_get=json.dumps(c.fetchall())
+	con.close
+	return jsonify(posts=json_get)
 
 
 if __name__ == '__main__':
