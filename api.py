17aabfcdc8088e43b293420221400850aa553714
williamx31v <williamx@easystreet.net>
2016-12-01 17:15:16 -0800

    Add files via upload

--- api.py
+++ api.py
@@ -10,56 +10,34 @@ from flask import request
 #define app
 app = Flask(__name__)
 
+#set the database path
 DATABASE='/home/william/blogpostapi/blog.db'
 
-
-def insert_blogpost(blogpost):
-	con=sqlite3.connect(DATABASE)
-	c=con.cursor()
-	c.execute("INSERT INTO posts (title,body) VALUES (?,?)")
-	con.commit()
-	con.close
 
 @app.route('/blogposts/api/v1.0/posts', methods=['GET','POST'])
 def result():
 	if request.method == 'POST':
-		blogpost == request.form['blogpost']
-		insert_blogpost(blogpost)
+		new_post = request.json
+		columns = ', '.join(new_post.keys())
+		vals = '"'+'", "'.join(new_post.values())
+		query = 'INSERT INTO posts (%s) VALUES (%s")' % (columns, vals)
+		con=sqlite3.connect(DATABASE)
+		c=con.cursor()
+		c.execute(query)
+		con.commit()
+		con.close
 		return "OK"
+
 	else:
 		con=sqlite3.connect(DATABASE)
 		c=con.cursor()
 		c.execute("SELECT * from posts")
 		json_get=json.dumps(c.fetchall())
 		con.close
-		return jsonify({'posts':json_get})
+		return json_get
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)	
 
-#get all records from table posts
-#@app.route('/blogposts/api/v1.0/posts', methods=['GET'])
-#def get_posts():
-#	if request.method == 'GET':	
-#		conn = sqlite3.connect("/home/william/blogpostapi/blog.db")
-#		c = conn.cursor()
-#		c.execute("SELECT * from posts")
-#		json_get=json.dumps(c.fetchall())
-#		conn.close
-#		return jsonify({'posts':json_get})
-#if __name__ == '__main__':
-#    app.run(debug=True,host='0.0.0.0',port=5000)
 
-#post into table posts
-#@app.route('/blogposts/api/v1.0/posts/post', methods=['POST'])
-#def posts_post():
-#	if not request.json or not 'title' or not 'body' in request.json:
-#       		abort(400)
-#	conn = sqlite3.connect("/home/william/blogpostapi/blog.db")
-#	json_post = request.json()
-#	c = conn.cursor()
-#	c.execute("INSERT into posts (title, body) VALUES (?,?)")
-#	conn.close
-#	return jsonify({'posts':json_post}), 201
-#if __name__ == '__main__':
-#    app.run(debug=True,host='0.0.0.0',port=5000)
